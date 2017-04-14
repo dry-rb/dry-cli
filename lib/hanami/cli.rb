a562c66e99d5d4333ce4214beef734420234c6f8
Luca Guidi <me@lucaguidi.com>
2017-04-14 10:59:14 +0200

    Minimal setup for Hanami::Cli

--- lib/hanami/cli.rb
+++ lib/hanami/cli.rb
@@ -2,6 +2,14 @@ require "hanami/cli/version"
 
 module Hanami
   module Cli
-    # Your code goes here...
+    def self.included(base)
+      mod = Module.new do
+        def self.call
+          puts "world"
+        end
+      end
+
+      base.const_set(:Cli, mod)
+    end
   end
 end
