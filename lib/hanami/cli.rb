564cf421cf47e906a2034bcfc04f159caf8650c3
Luca Guidi <me@lucaguidi.com>
2017-04-14 11:58:16 +0200

    Use a global registry to let third-party gems to override commands

--- lib/hanami/cli.rb
+++ lib/hanami/cli.rb
@@ -1,21 +1,35 @@
-require "hanami/cli/version"
+require "concurrent"
 
 module Hanami
   module Cli
+    require "hanami/cli/version"
+
     def self.included(base)
       mod = Module.new do
         def self.call(arguments: ARGV)
           cmd     = arguments.first
-          command = constants.find { |c| c.to_s.downcase == cmd }
+          command = Hanami::Cli.command(cmd)
           exit(1) if command.nil?
 
-          command = const_get(command)
-
           command.new.call
+        end
+
+        def self.register_as(name, command)
+          Hanami::Cli.register_as(name, command)
         end
       end
 
       base.const_set(:Cli, mod)
+    end
+
+    @__commands = Concurrent::Hash.new
+
+    def self.register_as(name, command)
+      @__commands[name] = command
+    end
+
+    def self.command(name)
+      @__commands.fetch(name, nil)
     end
   end
 end
--- spec/integration/basic_command_spec.rb
+++ spec/integration/basic_command_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe "Basic command" do
     expect(output).to match("world")
   end
 
+  it "prints version" do
+    output = `foo version`
+    expect(output).to match("1.0.0 yay!")
+  end
+
   it "fails for unknown command" do
     result = system("foo unknown")
     expect(result).to be(false)
