c749a07a90478dc845450c414b7a7f49971b6892
Luca Guidi <me@lucaguidi.com>
2017-04-14 11:08:07 +0200

    Basic commands lookup

--- lib/hanami/cli.rb
+++ lib/hanami/cli.rb
@@ -4,8 +4,11 @@ module Hanami
   module Cli
     def self.included(base)
       mod = Module.new do
-        def self.call
-          puts "world"
+        def self.call(arguments: ARGV)
+          cmd     = arguments.first
+          command = const_get(constants.find { |c| c.to_s.downcase == cmd })
+
+          command.new.call
         end
       end
 
