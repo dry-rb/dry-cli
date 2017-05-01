deb815184fce5dfe2f696f317fb77f4f68dc2523
Alfonso Uceda Pompa <alfonso@bebanjo.com>
2017-05-01 15:54:08 +0200

    Add aliases and desc

--- lib/hanami/cli.rb
+++ lib/hanami/cli.rb
@@ -31,12 +31,29 @@ module Hanami
     @__commands = Concurrent::Hash.new
 
     def self.register(name, command)
-      @__commands[name] = command
+      @__commands[name] ||= {}
+      @__commands[name][:command_class] = command
     end
 
     def self.command(arguments)
       command = arguments.join(" ")
-      @__commands.fetch(command, nil)
+      (@__commands[command] && @__commands[command][:command_class]) || command_by_alias(command)
+    end
+
+    def self.add_option(command_class, option)
+      @__commands.each do |command, values|
+        if values[:command_class] == command_class
+          @__commands[command].merge!(option)
+          break
+        end
+      end
+    end
+
+    private
+
+    def self.command_by_alias(command)
+      found_command = @__commands.detect{|_, values| values[:aliases].to_a.include?(command)}.to_a.last
+      found_command && found_command[:command_class]
     end
   end
 end
