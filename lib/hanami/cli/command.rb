7288023d1fde1796627fa1e43a6591bcbc995e7c
Alfonso Uceda Pompa <alfonso@bebanjo.com>
2017-06-04 17:58:41 +0200

    Convert Command class to module

--- lib/hanami/cli/command.rb
+++ lib/hanami/cli/command.rb
@@ -2,56 +2,64 @@ require "hanami/cli/param"
 
 module Hanami
   module Cli
-    class Command
-      attr_reader :options, :name
-
-      def initialize(name:, params:, arguments:)
-        @name = name
-        @params = params.to_a
-        @arguments = arguments
-        parse_params unless name.nil? || name.empty?
+    module Command
+      def self.included(base)
+        base.include InstanceMethods
+        base.extend ClassMethods
       end
 
-      def self.desc(description)
-        Hanami::Cli.add_option(self, desc: description)
-      end
+      module InstanceMethods
+        attr_reader :options, :name
 
-      def self.aliases(*names)
-        Hanami::Cli.add_option(self, aliases: names)
-      end
+        def initialize(name:, params:, arguments:)
+          @name = name
+          @params = params.to_a
+          @arguments = arguments
+          parse_params unless name.nil? || name.empty?
+        end
+        private
 
-      def self.param(name, options = {})
-        param = Param.new(name, options)
-        Hanami::Cli.add_param(self, param)
-      end
+        def parse_params
+          return unless @params
+          @options = {}
+          OptionParser.new do |opts|
+            opts.set_program_name("#{Pathname.new($0).basename} #{name}")
+            opts.separator("")
 
-      def self.register(*names)
-        names.each do |name|
-          Hanami::Cli.register(name, self)
+            @params.each do |param|
+              opts.on(*param.option_parser_options) do |value|
+                @options[param.name.to_sym] = value
+              end
+            end
+
+            opts.on_tail("-h", "--help", "Show this message") do
+              puts opts
+              exit
+            end
+          end.parse!(@arguments)
+        rescue OptionParser::InvalidOption
         end
       end
 
-      private
+      module ClassMethods
+        def desc(description)
+          Hanami::Cli.add_option(self, desc: description)
+        end
 
-      def parse_params
-        return unless @params
-        @options = {}
-        OptionParser.new do |opts|
-          opts.set_program_name("#{Pathname.new($0).basename} #{name}")
-          opts.separator("")
+        def aliases(*names)
+          Hanami::Cli.add_option(self, aliases: names)
+        end
 
-          @params.each do |param|
-            opts.on(*param.option_parser_options) do |value|
-              @options[param.name.to_sym] = value
-            end
+        def param(name, options = {})
+          param = Param.new(name, options)
+          Hanami::Cli.add_param(self, param)
+        end
+
+        def register(*names)
+          names.each do |name|
+            Hanami::Cli.register(name, self)
           end
-
-          opts.on_tail("-h", "--help", "Show this message") do
-            puts opts
-            exit
-          end
-        end.parse!(@arguments)
-      rescue OptionParser::InvalidOption
+        end
       end
     end
   end
