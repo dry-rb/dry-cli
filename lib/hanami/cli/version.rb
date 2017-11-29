40cffddbc299fbdd010e13187764963f06d7c395
Luca Guidi <me@lucaguidi.com>
2017-11-29 17:51:35 +0100

    Bump version to v1.0.0.alpha1

--- lib/hanami/cli/version.rb
+++ lib/hanami/cli/version.rb
@@ -3,6 +3,6 @@
 module Hanami
   class CLI
     # @since 0.1.0
-    VERSION = "0.1.0"
+    VERSION = "1.0.0.alpha1"
   end
 end
--- spec/unit/hanami/cli/version_spec.rb
+++ spec/unit/hanami/cli/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::CLI::VERSION" do
   it "exposes version" do
-    expect(Hanami::CLI::VERSION).to eq("0.1.0")
+    expect(Hanami::CLI::VERSION).to eq("1.0.0.alpha1")
   end
 end
