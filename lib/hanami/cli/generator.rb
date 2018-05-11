d9101d9c8d06ea833c66cd97dab2fda4e0fcef21
Sean Collins <sean@cllns.com>
2018-05-10 22:24:05 -0600

    Extract #generate_file to a new Generator class

---
+++ lib/hanami/cli/generator.rb
@@ -0,0 +1,71 @@
+require 'hanami/utils/files'
+require 'erb'
+
+module Hanami
+  class CLI
+    # A class to help generate files
+    #
+    # @since 0.1.0
+    class Generator
+      # @since x.x.x
+      # @api public
+      def initialize(out: $stdout, files: Utils::Files)
+        @files     = files
+        @out       = out
+      end
+
+      # @since 1.1.0
+      # @api private
+      def generate_file(source, destination, context)
+        files.write(
+          destination,
+          render(source, context)
+        )
+        say(:create, destination)
+      end
+
+      private
+
+      # @since x.x.x
+      # @api private
+      SAY_FORMATTER = "%<operation>12s  %<path>s\n".freeze
+
+      # @since x.x.x
+      # @api private
+      class Renderer
+        TRIM_MODE = "-".freeze
+
+        def initialize
+          freeze
+        end
+
+        def call(template, context)
+          ::ERB.new(template, nil, TRIM_MODE).result(context)
+        end
+      end
+
+      # @since x.x.x
+      # @api private
+      attr_reader :files
+
+      # @since x.x.x
+      # @api private
+      attr_reader :out
+
+      # @since x.x.x
+      # @api private
+      def render(path, context)
+        template = File.read(path)
+        renderer = Renderer.new
+
+        renderer.call(template, context.binding)
+      end
+
+      # @since x.x.x
+      # @api private
+      def say(operation, path)
+        out.puts(SAY_FORMATTER % { operation: operation, path: path }) # rubocop:disable Style/FormatString
+      end
+    end
+  end
+end
