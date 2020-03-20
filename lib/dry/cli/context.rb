50a7c61b7ffc488fd92fbf2846589f6801eaa519
Ivan Shamatov <status.enable@gmail.com>
2020-03-20 14:55:20 +0300

    Introducing context for Dry::CLI

---
+++ lib/dry/cli/context.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'forwardable'
+
+module Dry
+  class CLI
+    # Base class for Context
+    #
+    # @since 0.1.0
+    class Context
+      def initialize(**)
+      end
+    end
+  end
+end
