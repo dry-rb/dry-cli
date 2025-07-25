237aee3f9dce6021147230f6e06b55bd5ed691f2
Aaron Allen <hello@aaronmallen.me>
2025-07-25 01:54:24 -0500

    Refactor `example` DSL for better consistency and clarity
    Simplified the `example` DSL syntax in commands by replacing array-based
    declarations with clearer method calls. Updated tests and fixtures to
    align with the new structure, ensuring improved readability and
    maintainability.

--- lib/dry/cli/banner.rb
+++ lib/dry/cli/banner.rb
@@ -29,27 +29,69 @@ module Dry
       # @since 1.1.1
       # @api private
       def self.command_banner(command, name)
+        extended_arguments = extended_command_arguments(command)
+        extended_examples  = extended_command_examples(command, name)
+        extended_options   = extended_command_options(command)
+        indent = capture_indent(extended_arguments, extended_options, extended_examples)
+
         [
           command_name(name),
           command_name_and_arguments(command, name),
           command_description(command),
           command_subcommands(command),
-          command_arguments(command),
-          command_options(command),
-          command_examples(command, name)
+          command_arguments(extended_arguments, indent),
+          command_options(extended_options, indent),
+          command_examples(extended_examples, indent)
         ]
       end
 
       # @since 1.1.1
       # @api private
       def self.namespace_banner(namespace, name)
+        extended_options = extended_command_options(namespace)
+        indent = capture_indent([], extended_options, [])
+
         [
           command_name(name, "Namespace"),
           command_name_and_arguments(namespace, name),
           command_description(namespace),
           command_subcommands(namespace),
-          command_options(namespace)
+          command_options(extended_options, indent)
         ]
+      end
+
+      # @since unreleased
+      # @api private
+      def self.capture_indent(extended_arguments, extended_options, extended_examples)
+        strings = extended_arguments + extended_options + extended_examples
+        strings.map { |string, _| string.length }.max + 1
+      end
+
+      # @since unreleased
+      # @api private
+      def self.build_option_right(option)
+        description = option.desc
+        unless option.default.nil?
+          description = "#{description}, default: #{option.default.inspect}"
+        end
+        description
+      end
+
+      # @since unreleased
+      # @api private
+      def self.build_option_left(option)
+        name = Inflector.dasherize(option.name)
+        name = if option.boolean?
+                 "--[no-]#{name}"
+               elsif option.flag?
+                 "--#{name}"
+               elsif option.array?
+                 "--#{name}=VALUE1,VALUE2,.."
+               else
+                 "--#{name}=VALUE"
+               end
+        name = "#{name}, #{option.alias_names.join(", ")}" if option.aliases.any?
+        name
       end
 
       # @since 0.1.0
@@ -80,10 +122,13 @@ module Dry
 
       # @since 0.1.0
       # @api private
-      def self.command_examples(command, name)
-        return if command.examples.empty?
+      def self.command_examples(extended_examples, indent)
+        return if extended_examples.empty?
 
-        "\nExamples:\n#{command.examples.map { |example| "  #{name} #{example}" }.join("\n")}"
+        examples = extended_examples.map { |example, description|
+          "  #{example.ljust(indent)} # #{description}"
+        }
+        "\nExamples:\n#{examples.join("\n")}"
       end
 
       # @since 0.1.0
@@ -102,16 +147,22 @@ module Dry
 
       # @since 0.1.0
       # @api private
-      def self.command_arguments(command)
-        return if command.arguments.empty?
+      def self.command_arguments(extended_arguments, indent)
+        return if extended_arguments.empty?
 
-        "\nArguments:\n#{extended_command_arguments(command)}"
+        arguments = extended_arguments.map { |argument, description|
+          "  #{argument.ljust(indent)} # #{description}"
+        }
+        "\nArguments:\n#{arguments.join("\n")}"
       end
 
       # @since 0.1.0
       # @api private
-      def self.command_options(command)
-        "\nOptions:\n#{extended_command_options(command)}"
+      def self.command_options(extended_options, indent)
+        options = extended_options.map { |option, description|
+          "  #{option.ljust(indent)} # #{description}"
+        }
+        "\nOptions:\n#{options.join("\n")}"
       end
 
       # @since 0.1.0
@@ -131,34 +182,26 @@ module Dry
       # @api private
       def self.extended_command_arguments(command)
         command.arguments.map do |argument|
-          "  #{argument.name.to_s.upcase.ljust(32)}  # #{"REQUIRED " if argument.required?}#{argument.desc}"
-        end.join("\n")
+          [argument.name.to_s.upcase, "#{"REQUIRED " if argument.required?}#{argument.desc}"]
+        end
+      end
+
+      # @since 0.1.0
+      # @api private
+      def self.extended_command_examples(command, name)
+        command.examples.map do |example, description|
+          ["#{name} #{example}", description]
+        end
       end
 
       # @since 0.1.0
       # @api private
       #
       def self.extended_command_options(command)
-        result = command.options.map do |option|
-          name = Inflector.dasherize(option.name)
-          name = if option.boolean?
-                   "[no-]#{name}"
-                 elsif option.flag?
-                   name
-                 elsif option.array?
-                   "#{name}=VALUE1,VALUE2,.."
-                 else
-                   "#{name}=VALUE"
-                 end
-          name = "#{name}, #{option.alias_names.join(", ")}" if option.aliases.any?
-          name = "  --#{name.ljust(30)}"
-          name = "#{name}  # #{option.desc}"
-          name = "#{name}, default: #{option.default.inspect}" unless option.default.nil?
-          name
-        end
-
-        result << "  --#{"help, -h".ljust(30)}  # Print this help"
-        result.join("\n")
+        result = command.options.map { |option|
+          [build_option_left(option), build_option_right(option)]
+        }
+        result << ["--help, -h", "Print this help"]
       end
 
       def self.build_subcommands_list(subcommands)
--- spec/support/shared_examples/inherited_commands.rb
+++ spec/support/shared_examples/inherited_commands.rb
@@ -43,7 +43,7 @@ RSpec.shared_examples "Inherited commands" do |cli|
           sub-command                       # I'm a concrete command
 
         Options:
-          --help, -h                        # Print this help
+          --help, -h  # Print this help
       DESC
       expect(output).to eq(expected)
     end
@@ -61,12 +61,12 @@ RSpec.shared_examples "Inherited commands" do |cli|
           Run a one-off process inside your app
 
         Arguments:
-          APP                               # REQUIRED Application name
-          CMD                               # REQUIRED Command to execute
+          APP                # REQUIRED Application name
+          CMD                # REQUIRED Command to execute
 
         Options:
-          --verbosity=VALUE                 # Verbosity level, default: "INFO"
-          --help, -h                        # Print this help
+          --verbosity=VALUE  # Verbosity level, default: "INFO"
+          --help, -h         # Print this help
       DESC
       expect(output).to eq(expected)
     end
@@ -81,12 +81,12 @@ RSpec.shared_examples "Inherited commands" do |cli|
           #{cmd} i subrun APP CMD
 
         Arguments:
-          APP                               # REQUIRED Application name
-          CMD                               # REQUIRED Command to execute
+          APP                # REQUIRED Application name
+          CMD                # REQUIRED Command to execute
 
         Options:
-          --verbosity=VALUE                 # Verbosity level, default: "INFO"
-          --help, -h                        # Print this help
+          --verbosity=VALUE  # Verbosity level, default: "INFO"
+          --help, -h         # Print this help
       DESC
       expect(output).to eq(expected)
     end
@@ -104,16 +104,16 @@ RSpec.shared_examples "Inherited commands" do |cli|
           Lists your add-ons and attachments
 
         Arguments:
-          APP                               # REQUIRED Application name
+          APP                             # REQUIRED Application name
 
         Options:
-          --verbosity=VALUE                 # Verbosity level, default: "INFO"
-          --[no-]json                       # return add-ons in json format, default: false
-          --help, -h                        # Print this help
+          --verbosity=VALUE               # Verbosity level, default: "INFO"
+          --[no-]json                     # return add-ons in json format, default: false
+          --help, -h                      # Print this help
 
         Examples:
-          #{cmd} i addons APP_NAME
-          #{cmd} i addons APP_NAME --json
+          #{cmd} i addons APP_NAME         # Basic usage
+          #{cmd} i addons APP_NAME --json  # Return add-ons in JSON format
       DESC
       expect(output).to eq(expected)
     end
@@ -131,18 +131,18 @@ RSpec.shared_examples "Inherited commands" do |cli|
           Display recent log output
 
         Arguments:
-          APP                               # REQUIRED Application name
+          APP                             # REQUIRED Application name
 
         Options:
-          --verbosity=VALUE                 # Verbosity level, default: "INFO"
-          --num=VALUE                       # number of lines to display
-          --[no-]tail                       # continually stream log
-          --help, -h                        # Print this help
+          --verbosity=VALUE               # Verbosity level, default: "INFO"
+          --num=VALUE                     # number of lines to display
+          --[no-]tail                     # continually stream log
+          --help, -h                      # Print this help
 
         Examples:
-          #{cmd} i logs APP_NAME
-          #{cmd} i logs APP_NAME --num=50
-          #{cmd} i logs APP_NAME --tail
+          #{cmd} i logs APP_NAME           # Basic usage
+          #{cmd} i logs APP_NAME --num=50  # Display 50 lines
+          #{cmd} i logs APP_NAME --tail    # Continually stream log
       DESC
       expect(output).to eq(expected)
     end
